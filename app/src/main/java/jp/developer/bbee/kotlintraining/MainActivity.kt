b691a80827a9db88fac25c9e01284f5df2a51d0c
BBee <atsushi.kokubun.47@gmail.com>
2022-04-20 01:18:39 +0900

    KotlinTraining: SearchView

--- app/src/main/java/jp/developer/bbee/kotlintraining/MainActivity.kt
+++ app/src/main/java/jp/developer/bbee/kotlintraining/MainActivity.kt
@@ -2,10 +2,7 @@ package jp.developer.bbee.kotlintraining
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,13 +13,31 @@ class MainActivity : AppCompatActivity() {
             "Saffron", "Nutmeg", "Cinnamon", "Pepper powder", "Turmeric powder", "Coriander powder", "Ginger powder", "Garlic powder",
             "Saffron powder", "Nutmeg powder", "Cinnamon powder")
 
-        val list = findViewById<ListView>(R.id.lvSpice);
+        val list = findViewById<ListView>(R.id.lvSpice)
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, data)
         list.adapter = adapter
+        list.isTextFilterEnabled = true
 
         list.setOnItemClickListener{ av, view, position, id ->
             //adapter.remove((view as TextView).text as String)
             Toast.makeText(this@MainActivity, "${(view as TextView).text}", Toast.LENGTH_SHORT).show()
         }
+
+        val sv = findViewById<SearchView>(R.id.svSpice)
+        sv.setOnQueryTextListener(
+            object : SearchView.OnQueryTextListener {
+                override fun onQueryTextChange(text: String?): Boolean {
+                    if (text.isNullOrBlank()) {
+                        list.clearTextFilter()
+                    } else {
+                        list.setFilterText(text)
+                    }
+                    return false
+                }
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return false
+                }
+            }
+        )
     }
 }
